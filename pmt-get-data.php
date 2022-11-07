45e4fde2d382441b2813c07b44c6b9d66f30d1fa
sekper <sekper@noreply@ptpn4.co.id>
2022-11-07 04:30:58 +0000

    Add 'pmt-get-data.php'

---
+++ pmt-get-data.php
@@ -0,0 +1,16 @@
+<?php
+error_reporting(E_ALL);
+$host = "localhost";
+$database = "humasptpn_data-pmt";
+        $username = "humasptpn_adminPMTPN4";
+        $password = "PMTPN4#@!admin";
+        $connect = mysqli_connect($host, $username, $password, $database);
+        $query = 'select* from stok_sparepart_PMT';
+        $result = mysqli_query($connect, $query);
+        while ($row = mysqli_fetch_assoc($result)) {
+                $return_arr[] = $row;
+        }
+
+echo json_encode($return_arr);
+mysqli_close($connect);
+?>
