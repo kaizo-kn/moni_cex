19f366dca141bbad43d84ba308707f23a7eb0d44
sekper <sekper@noreply@ptpn4.co.id>
2022-11-09 04:38:56 +0000

    Upload files to 'pmt_dolok_ilir'

---
+++ pmt_dolok_ilir/footer.php
@@ -0,0 +1,8 @@
+<?php echo '<section id="footer">
+        <div class="row mb-5">
+            <div class="col  text-center">
+                <h4 style="font-weight:bold;color:black;font-style:normal;text-align:center">Contact Person:</h4>
+                <h4 style="font-weight:bold;color:black;font-style:normal;text-align:center">[PMT Dolok Ilir] | [0813-9660-3334 / Suhardiman ]</h4>
+            </div>
+        </div>
+    </section>';?>
