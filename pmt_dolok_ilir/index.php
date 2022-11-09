a55c7287e97fd8e095472b3cf2a636ee54a7a84d
sekper <sekper@noreply@ptpn4.co.id>
2022-11-09 04:02:44 +0000

    Upload files to 'pmt_dolok_ilir'

---
+++ pmt_dolok_ilir/index.php
@@ -0,0 +1,297 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Beranda - PMT Dolok Ilir</title>
+    <link rel="icon" type="image/x-icon" href="assets/icon/Logo_PTPN4.png">
+    <!-- Offline -->
+    <link rel="stylesheet" href="assets/css/main-style.css">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.css">
+    <script src="assets/bootstrap/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.bundle.js"></script>
+    
+
+</head>
+
+<body>
+    <section id="header">
+        <div class="container">
+            <div class="header mb-sm-5" style="background: url('assets/images/header-pmt.jpg') center center no-repeat;">
+                <div class="container d-flex align-content-center mt-5 pt-5 ">
+                    <div class="text-light text-center">
+                        <h4 class="font-weight-lighter">
+                            Program 6 Standarisasi Mesin Instalasi PKS Dimulai pada Tahun 2022 Oleh Bagian Teknik dan Pengolahan PTPN 4
+                        </h4>
+                        <h1 class="font-weight-bold">
+                            6 Standarisasi dan Penyediaan Stok dan Komponen Mesin Instalasi PKS
+                        </h1>
+                    </div>
+                </div>
+            </div>
+            <div class="container p-4 text-center text-dark mt-3" style="background-color: orange;">
+                <h2>Tujuan 6 Standarisasi Mesin dan Instalasi di PKS
+                </h2><br><br>
+                <p class="font-weight-bold">Program 6 Standarisasi ini bertujuan : kualitas bahan yang dipakai, kualitas pekerjaan dan kualitas pemasangan yang lebih terjamin sehingga umur teknis lebih tinggi; performance meningkat (kapasitas dan mutu olah tercapai); dan memudahkan
+                    PMT Dolok Ilir dalam menyediakan suku cadang/ spare part.</p>
+            </div>
+            <div class="row d-flex justify-content-center pt-lg-0 mt-lg-0 pb-5 mb-5 pt-sm-5">
+                <div class="col-lg-5 col-xl-5 col-sm-12 mt-lg-5">
+                    <div class="box-wrapper__ mb-sm-5" onclick="$('#hdc').toggle()">
+                        <div class="box__ box__-down cyan__">
+                            <div style="display: flex;justify-content:center;height:250px;width:auto;margin-top:15px">
+                                <img style="transform: scale(0.8);" src="https://www.ptpn4.co.id/wp-content/uploads/2022/10/isometric-e1667276965398.png" alt="">
+                            </div>
+                            <b></b>
+                            <h2>Hydrocyclone</h2>
+                            <p>Hydrocyclone adalah alat pemisah inti dan cangkang dalam cracksheel dari LTDS (Light Tenera Dust Separator) menggunakan media air.
+
+                            </p>
+                            <br><br><br><br><br>
+                            <p class="text-center">Klik untuk melihat spesifikasi</p>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-5 col-xl-5 col-sm-12 mt-lg-5">
+                    <div class="box-wrapper__ mb-sm-5" onclick="$('#cbc').toggle()">
+                        <div class="box__ box__-down cyan__">
+                            <div style="display: flex;justify-content:center;height:250px;width:auto;margin-top:15px">
+                                <img style="transform: scale(0.8);" src="https://www.ptpn4.co.id/wp-content/uploads/2022/10/cbc-e1666766820728.png" alt="">
+                            </div>
+                            <b></b>
+                            <h2>Cake Breaker Conveyor</h2>
+                            <p>Cake breaker conveyor (CBC) adalah alat penghantar dan pemecah ampas kempa (sekaligus mengeringkannya) dari pressan ke Depericarper. Hasil proses di CBC menjadikan biji dan fibre terurai (tidak menggumpal) dan lebih kering.
+                            </p>
+                            <p class="text-center">Klik untuk melihat spesifikasi</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-5 col-xl-5 col-sm-12 mt-lg-5">
+                    <div class="box-wrapper__ mb-sm-5" onclick="$('#fle').toggle()">
+                        <div class="box__ box__-down cyan__">
+                            <div style="display: flex;justify-content:center;height:250px;width:auto;margin-top:15px">
+                                <img width="100px" height="200px" src="https://www.ptpn4.co.id/wp-content/uploads/2022/10/fruitelev-e1667277149606.png" alt="">
+                            </div>
+                            <b></b>
+                            <h2>Fruit Elevator</h2>
+                            <p>Fruit elevator adalah alat angkut yang memindahkan berondolan rebus dari elevasi rendah ke elevasi tinggi. Bagian utama dari fruit elevator yaitu: body, bucket dan rantai. </p>
+                            <br>
+                            <br>
+                            <p class="text-center">Klik untuk melihat spesifikasi</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-5 col-xl-5 col-sm-12 mt-lg-5">
+                    <div class="box-wrapper__ mb-sm-5" onclick="$('#lori').toggle()">
+                        <div class="box__ box__-down cyan__">
+                            <div style="display: flex;justify-content:center;height:250px;width:auto;margin-top:15px">
+                                <img style="transform: scale(0.8);" src="https://www.ptpn4.co.id/wp-content/uploads/2022/10/lori-e1666766982504.png" alt="">
+                            </div>
+                            <b></b>
+                            <h2>Fruit Cages/ Lori Buah</h2>
+                            <p>Fruit Cage (Lori Buah) adalah komponen Pabrik Kelapa Sawit (PKS) yang berfungsi sebagai alat yang mengangkut Tandan Buah Segar (TBS) dari Loading Ramp yang nantinya dimasukkan kedalam Sterilizer untuk dilakukan proses perebusan
+                                (masak). </p>
+
+                            <p class="text-center">Klik untuk melihat spesifikasi</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-5 col-xl-5 col-sm-12 mt-lg-5">
+                    <div class="box-wrapper__ mb-sm-5" onclick="$('#pld').toggle()">
+                        <div class="box__ box__-down cyan__">
+                            <div style="display: flex;justify-content:center;height:250px;width:auto;margin-top:15px">
+                                <img style="transform: scale(0.8);" src="https://www.ptpn4.co.id/wp-content/uploads/2022/10/polishdrum-e1667277181789.png" alt="">
+                            </div>
+                            <h2>Polishing Drum</h2>
+                            <p>Polishing drum adalah tromol berputar yang berfungsi untuk memolish / membersihkan sisa-sisa serabut yang masih lengket pada permukaan biji
+
+                            </p>
+                            <br><br><br>
+                            <p class="text-center">Klik untuk melihat spesifikasi</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-5 col-xl-5 col-sm-12 mt-lg-5">
+                    <div class="box-wrapper__ mb-sm-5" onclick="$('#thrs').toggle()">
+                        <div class="box__ box__-down cyan__">
+                            <div style="display: flex;justify-content:center;height:250px;width:auto;margin-top:15px">
+                                <img style="transform: scale(0.8);" width="auto" height="auto" src="https://www.ptpn4.co.id/wp-content/uploads/2022/10/thresher-e1667277214452.png" alt="">
+                            </div>
+                            <b></b>
+                            <h2>Thresher</h2>
+                            <p>Tresher berfungsi untuk memisahkan brondolan dengan tandan kosong dengan cara membanting dalam drum yang berputar. Tresher terdiri dari sebuah drum yang dindingnya terbentuk dari kisi-kisi dengan jarak tertentu, dan dilengkapi
+                                dengan sirip untuk mengangkat janjangan dan membawanya ke ujung drum untuk dikeluarkan.
+                            </p>
+                            </p>
+
+                            <p class="text-center">Klik untuk melihat spesifikasi</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+    </section>
+    <section id="maps">
+        <div class="row mt-lg-5 pt-lg-5 pb-lg-5 pt-sm-0 mt-sm-1">
+            <div class="container d-flex justify-content-center mt-lg-5 pt-lg-5 pb-5 pt-sm-0 ">
+                <iframe style="border:1px solid rgb(112, 112, 112);border-radius: 5px;" width="100%" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3983.901765039673!2d99.15984856807069!3d3.1206798541886043!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30317e69a3363351%3A0xe534e90602846ee2!2sPTPN%20IV%20Dolok%20Ilir!5e0!3m2!1sid!2sid!4v1666852086519!5m2!1sid!2sid"
+                    height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+        </div>
+    </section>
+    <section id="contact-person">
+        <div class="row mb-5">
+            <div class="col  text-center">
+                <h4 style="font-weight:bold;color:black;font-style:normal;text-align:center">Contact Person:</h4>
+                <h4 style="font-weight:bold;color:black;font-style:normal;text-align:center">[PMT Dolok Ilir] | [0813-9660-3334 / Suhardiman ]</h4>
+            </div>
+        </div>
+    </section>
+
+    <section id="modal__">
+        <div id="cbc" onclick="document.getElementById('cbc').style.display='none'" class="modal__">
+            <div class="modal-content__">
+                <div class="modal-header__">
+                    <span onclick="document.getElementById('cbc').style.display = 'none'" class="close__">×</span>
+                    <h2 style="text-align:center ;color:white;">Cake Breaker Conveyor</h2>
+                </div>
+                <div class="modal-body__">
+                    <div>
+                        <img src="https://www.ptpn4.co.id/wp-content/uploads/2022/10/CBC.jpg" alt="" srcset="">
+                    </div>
+                    <div>
+                        <img src="https://www.ptpn4.co.id/wp-content/uploads/2022/10/CBC-2.jpg" alt="" srcset="">
+                    </div>
+                </div>
+                <div class="modal-footer__">
+                    <h3 style="text-align:center ;color:white;">PMT PTPN IV Dolok Ilir</h3>
+                </div>
+            </div>
+        </div>
+        <div id="hdc" class="modal__">
+            <div class="modal-content__">
+                <div class="modal-header__">
+                    <span onclick="document.getElementById('hdc').style.display = 'none'" class="close__">×</span>
+                    <h2 style="text-align:center ;color:white;">Hydrocyclone</h2>
+                </div>
+                <div class="modal-body__">
+                    <div>
+                        <img src="https://www.ptpn4.co.id/wp-content/uploads/2017/01/HIDROCYCLONE-2-2.jpg" alt="" srcset="">
+                    </div>
+                    <div>
+                        <img src="https://www.ptpn4.co.id/wp-content/uploads/2022/11/HIDROCYCLONE-2-3.jpg" alt="" srcset="">
+                    </div>
+                </div>
+                <div class="modal-footer__">
+                    <h3 style="text-align:center ;color:white;">PMT PTPN IV Dolok Ilir</h3>
+                </div>
+            </div>
+        </div>
+        <div id="cbc" class="modal__">
+            <div class="modal-content__">
+                <div class="modal-header__">
+                    <span onclick="document.getElementById('cbc').style.display = 'none'" class="close__">×</span>
+                    <h2 style="text-align:center ;color:white;">Cake Breaker Conveyor</h2>
+                </div>
+                <div class="modal-body__">
+
+                    <img src="https://www.ptpn4.co.id/wp-content/uploads/2022/10/CBC.jpg" alt="" srcset="">
+
+
+                    <img src="https://www.ptpn4.co.id/wp-content/uploads/2022/10/CBC-2.jpg" alt="" srcset="">
+
+                </div>
+                <div class="modal-footer__">
+                    <h3 style="text-align:center ;color:white;">PMT PTPN IV Dolok Ilir</h3>
+                </div>
+            </div>
+
+        </div>
+        <div id="fle" class="modal__">
+            <div class="modal-content__">
+                <div class="modal-header__">
+                    <span onclick="document.getElementById('fle').style.display = 'none'" class="close__">×</span>
+                    <h2 style="text-align:center ;color:white;">Fruit Elevator</h2>
+                </div>
+                <div class="modal-body__">
+                    <div>
+                        <img src="https://www.ptpn4.co.id/wp-content/uploads/2022/10/FRUIT-ELEVATOR.jpg" alt="" srcset="">
+                    </div>
+                    <div>
+                        <img src="https://www.ptpn4.co.id/wp-content/uploads/2022/10/FRUIT-ELEVATOR-2.jpg" alt="" srcset="">
+                    </div>
+                </div>
+                <div class="modal-footer__">
+                    <h3 style="text-align:center ;color:white;">PMT PTPN IV Dolok Ilir</h3>
+                </div>
+            </div>
+
+        </div>
+        <div id="lori" class="modal__">
+            <div class="modal-content__">
+                <div class="modal-header__">
+                    <span onclick="document.getElementById('lori').style.display = 'none'" class="close__">×</span>
+                    <h2 style="text-align:center ;color:white;">Fruit Cages/Lori Buah</h2>
+                </div>
+                <div class="modal-body__">
+                    <div>
+                        <img src="https://www.ptpn4.co.id/wp-content/uploads/2022/10/LORI.jpg" alt="" srcset="">
+                    </div>
+                    <div>
+                        <img src="https://www.ptpn4.co.id/wp-content/uploads/2022/10/LORI-2.jpg" alt="" srcset="">
+                    </div>
+                </div>
+                <div class="modal-footer__">
+                    <h3 style="text-align:center ;color:white;">PMT PTPN IV Dolok Ilir</h3>
+                </div>
+            </div>
+
+        </div>
+        <div id="pld" class="modal__">
+            <div class="modal-content__">
+                <div class="modal-header__">
+                    <span onclick="document.getElementById('pld').style.display = 'none'" class="close__">×</span>
+                    <h2 style="text-align:center ;color:white;">Polishing Drum</h2>
+                </div>
+                <div class="modal-body__">
+                    <div>
+                        <img src="https://www.ptpn4.co.id/wp-content/uploads/2022/10/POLISHING-DRUM.jpg" alt="" srcset="">
+                    </div>
+                    <div>
+                        <img src="https://www.ptpn4.co.id/wp-content/uploads/2022/10/POLISHING-DRUM-2-1.jpg" alt="" srcset="">
+                    </div>
+                </div>
+                <div class="modal-footer__">
+                    <h3 style="text-align:center ;color:white;">PMT PTPN IV Dolok Ilir</h3>
+                </div>
+            </div>
+
+        </div>
+        <div id="thrs" class="modal__">
+            <div class="modal-content__">
+                <div class="modal-header__">
+                    <span onclick="document.getElementById('thrs').style.display = 'none'" class="close__">×</span>
+                    <h2 style="text-align:center ;color:white;">Thresher</h2>
+                </div>
+                <div class="modal-body__">
+                    <div>
+                        <img src="https://www.ptpn4.co.id/wp-content/uploads/2022/10/THRESHER.jpg" alt="" srcset="">
+                    </div>
+                    <div>
+                        <img src="https://www.ptpn4.co.id/wp-content/uploads/2022/10/THRESHER-2.jpg" alt="" srcset="">
+                    </div>
+                </div>
+                <div class="modal-footer__">
+                    <h3 style="text-align:center ;color:white;">PMT PTPN IV Dolok Ilir</h3>
+                </div>
+            </div>
+
+        </div>
+    </section>
+</body>
+
+</html>
