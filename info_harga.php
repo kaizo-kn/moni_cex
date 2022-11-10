1048512ca1e48e1920c840cff1ec1ada68e39517
Prayogi. W <email.prayogi.wiranata@gmail.com>
2022-11-10 10:47:01 +0700

    new commit

---
+++ info_harga.php
@@ -0,0 +1,238 @@
+<?php 
+$page_title='Informasi Harga Tiap Mesin Instalasi';
+include 'head.php';
+?>
+<div class="container">
+    <div style="font-size:2.4em" class="text-center text-dark font-weight-bold p-5 pt-2 mb-1 mt-3">
+        Informasi Harga Tiap Mesin Instalasi
+
+    </div>
+    <div>
+        <div class="text-center mb-5">
+            <img class="scale-with-grid" src="https://www.ptpn4.co.id/wp-content/uploads/2022/11/PMT-DOI-10-NOVEMBER-2022.jpg" alt="PPT"
+                height="500px" width="auto">
+            <div class="ml-5">
+                <div class="text-left ml-5 pl-5 p-3">
+                    <h5><b>CATATAN:</b></h5>
+                    <ul style="color:black;font-size:1em;">
+                        <li>Harga BELUM termasuk biaya pengiriman (transportasi)</li>
+                        <li>Harga sudah termasuk PPN 11% dan upah</li>
+                        <li>Harga dapat berubah sewaktu-waktu sesuai kondisi pasar</li>
+                        <li>Niaya belum termasuk tarif pekerja atau mandah di LUAR SUMATERA UTARA (luar wilayah)</li>
+                        <li>Pemesanan selain PTPN IV harus melalui persetujuan BoM (Board of Management)</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="text-center">
+        <p class="lead font-weight-bold"> Rincian Detail Bahan dan Harga Setiap 6 Standarisasi Mesin - Mesin Proses
+            Kelapa Sawit</p>
+    </div>
+    <div class="row d-flex justify-content-center">
+        <div class="col-lg-2 col-sm-6">
+            <div class="pricebox-wrapper__" onclick="$('#lori-h').toggle()"
+                style="background-image:url('https://www.ptpn4.co.id/wp-content/uploads/2022/10/lori-e1666766982504.png');">
+                <div class="pricebox__">
+                    <img src="" alt="" srcset="">
+                    <h4 class="pricebox-brand__">Lori</h4>
+                </div>
+
+            </div>
+            <div id="lori-h" onclick="document.getElementById('lori-h').style.display='none'" class="modal__">
+                <!-- Modal content -->
+                <div class="modal-content__">
+                    <div class="modal-header__">
+                        <span onclick="document.getElementById('lori-h').style.display = 'none'"
+                            class="close__">×</span>
+                        <h2 style="text-align:center ;color:white;">Lori</h2>
+                    </div>
+                    <div class="modal-body__">
+                        <div>
+                            <img src="https://www.ptpn4.co.id/wp-content/uploads/2017/01/LORRY.png" alt="" srcset="">
+                        </div>
+                    </div>
+                    <div class="modal-footer__">
+                        <h3 style="text-align:center ;color:white;">PMT PTPN IV Dolok Ilir</h3>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <div class="col-lg-2 col-sm-6">
+            <div onclick="$('#thrs-h').toggle()" class="pricebox-wrapper__"
+                style="background-image:url('https://www.ptpn4.co.id/wp-content/uploads/2022/10/thresher.png')">
+                <div class="pricebox__">
+                    <h4 class="pricebox-brand__">Thresher</h4>
+                </div>
+            </div>
+            <div id="thrs-h" onclick="document.getElementById('thrs-h').style.display='none'" class="modal__">
+                <!-- Modal content -->
+                <div class="modal-content__">
+                    <div class="modal-header__">
+                        <span onclick="document.getElementById('thrs-h').style.display = 'none'"
+                            class="close__">×</span>
+                        <h2 style="text-align:center ;color:white;">Thresher</h2>
+                    </div>
+                    <div class="modal-body__">
+                        <div>
+                            <img src="https://www.ptpn4.co.id/wp-content/uploads/2022/10/thresher-1.png" alt=""
+                                srcset="">
+                        </div>
+                        <div>
+                            <img src="https://www.ptpn4.co.id/wp-content/uploads/2022/10/thresher-2.png" alt=""
+                                srcset="">
+                        </div>
+                    </div>
+                    <div class="modal-footer__">
+                        <h3 style="text-align:center ;color:white;">PMT PTPN IV Dolok Ilir</h3>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <div class="col-lg-2 col-sm-6">
+            <div onclick="$('#cbc-h').toggle()" class="pricebox-wrapper__"
+                style="background-image:url('https://www.ptpn4.co.id/wp-content/uploads/2022/10/cbc-e1666766820728.png');">
+
+                <div class="pricebox__"
+                    style="width:100% ;height:100px!important;display: flex;align-items: center;justify-content: center;">
+                    <h3 class="pricebox-brand__">CBC</h3>
+                </div>
+            </div>
+            <div id="cbc-h" onclick="$('.modal__').css('display','none')" class="modal__">
+                <!-- Modal content -->
+                <div class="modal-content__">
+                    <div class="modal-header__">
+                        <span onclick="document.getElementById('cbc-h').style.display = 'none'" class="close__">×</span>
+                        <h2 style="text-align:center ;color:white;">Cake Breaker Conveyor</h2>
+                    </div>
+                    <div class="modal-body__">
+                        <div>
+                            <img src="https://www.ptpn4.co.id/wp-content/uploads/2017/01/CBCA.png"
+                                alt="Cake Breaker Conveyor" srcset="">
+                        </div>
+                        <div>
+                            <img src="https://www.ptpn4.co.id/wp-content/uploads/2017/01/CBC.B.png"
+                                alt="Cake Breaker Conveyor" srcset="">
+                        </div>
+                    </div>
+                    <div class="modal-footer__">
+                        <h3 style="text-align:center ;color:white;">PMT PTPN IV Dolok Ilir</h3>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <div class="col-lg-2 col-sm-6">
+            <div onclick="$('#fre-h').toggle()" class="pricebox-wrapper__"
+                style="background-image:url('https://www.ptpn4.co.id/wp-content/uploads/2022/10/fruitelev.png');">
+                <div class="pricebox__">
+                    <h4 class="pricebox-brand__">Timba Buah</h4>
+                </div>
+
+            </div>
+            <div id="fre-h" onclick="document.getElementById('fre-h').style.display='none'" class="modal__">
+                <!-- Modal content -->
+                <div class="modal-content__">
+                    <div class="modal-header__">
+                        <span onclick="document.getElementById('fre-h').style.display = 'none'" class="close__">×</span>
+                        <h2 style="text-align:center ;color:white;">Timba Buah</h2>
+                    </div>
+                    <div class="modal-body__">
+                        <div>
+                            <img src="https://www.ptpn4.co.id/wp-content/uploads/2022/10/timba-buah.png" alt=""
+                                srcset="">
+                        </div>
+                        <div>
+                            <img src="https://www.ptpn4.co.id/wp-content/uploads/2022/10/timba-buah-2.png" alt=""
+                                srcset="">
+                        </div>
+                    </div>
+                    <div class="modal-footer__">
+                        <h3 style="text-align:center ;color:white;">PMT PTPN IV Dolok Ilir</h3>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <div class="col-lg-2 col-sm-6">
+            <div onclick="$('#hdc-h').toggle()" class="pricebox-wrapper__"
+                style="background-image:url('https://www.ptpn4.co.id/wp-content/uploads/2022/10/isometric.png');">
+                <div class="pricebox__">
+                    <h4 class="pricebox-brand__">Hidro<br>Cyclone</h4>
+
+                </div>
+            </div>
+
+            <div id="hdc-h" onclick="document.getElementById('hdc-h').style.display='none'" class="modal__">
+                <!-- Modal content -->
+                <div class="modal-content__">
+                    <div class="modal-header__">
+                        <span onclick="document.getElementById('hdc-h').style.display = 'none'" class="close__">×</span>
+                        <h2 style="text-align:center ;color:white;">Hidrocyclone</h2>
+                    </div>
+                    <div class="modal-body__">
+                        <div>
+                            <img width="1080px"
+                                src="https://www.ptpn4.co.id/wp-content/uploads/2022/11/row-1-column-1.png" alt=""
+                                srcset="">
+                        </div>
+                        <div>
+                            <img width="1080px"
+                                src="https://www.ptpn4.co.id/wp-content/uploads/2022/11/row-2-column-1.png" alt=""
+                                srcset="">
+                        </div>
+                        <div>
+                            <img width="1080px"
+                                src="https://www.ptpn4.co.id/wp-content/uploads/2022/11/row-3-column-1.png" alt=""
+                                srcset="">
+                        </div>
+                    </div>
+                    <div class="modal-footer__">
+                        <h3 style="text-align:center ;color:white;">PMT PTPN IV Dolok Ilir</h3>
+                    </div>
+                </div>
+
+
+
+            </div>
+
+
+        </div>
+        <div class="col-lg-2 col-sm-6">
+            <div onclick="$('#pld-h').toggle()" class="pricebox-wrapper__"
+                style="background-image:url('https://www.ptpn4.co.id/wp-content/uploads/2022/10/polishdrum.png');">
+                <div class="pricebox__">
+                    <h5 class="pricebox-brand__">Polishing Drum</h5>
+
+                </div>
+            </div>
+
+            <div id="pld-h" onclick="document.getElementById('pld-h').style.display='none'" class="modal__">
+                <!-- Modal content -->
+                <div class="modal-content__">
+                    <div class="modal-header__">
+                        <span onclick="document.getElementById('pld-h').style.display = 'none'" class="close__">×</span>
+                        <h2 style="text-align:center ;color:white;">Polishing Drum</h2>
+                    </div>
+                    <div class="modal-body__">
+                        <div>
+                            <img src="https://www.ptpn4.co.id/wp-content/uploads/2017/01/polishing-drum-A1-e1667279335920.png"
+                                alt="" srcset="">
+                        </div>
+                        <div>
+                            <img src="https://www.ptpn4.co.id/wp-content/uploads/2017/01/polishing-drum-B-e1667279223245.png"
+                                alt="" srcset="">
+                        </div>
+                    </div>
+                    <div class="modal-footer__">
+                        <h3 style="text-align:center ;color:white;">PMT PTPN IV Dolok Ilir</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include 'footer.php' ?>
