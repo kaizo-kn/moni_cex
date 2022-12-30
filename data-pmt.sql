9bb53d1eed6a6e92c8f5836e74a518d5f8fdc197
Prayogi. W <email.prayogi.wiranata@gmail.com>
2022-12-30 12:25:17 +0700

    new db dump

--- data-pmt.sql
+++ data-pmt.sql
@@ -3,13 +3,13 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 29, 2022 at 04:33 AM
+-- Generation Time: Dec 30, 2022 at 06:23 AM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 7.4.27
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
-SET time_zone = "+07:00";
+SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -50,15 +50,17 @@ INSERT INTO `balasan_komplain` (`id_balasan`, `tanggal_balasan`, `balasan`, `gam
 --
 
 CREATE TABLE `catatan_info_harga` (
-  `catatan` text NOT NULL
+  `id` int(11) NOT NULL,
+  `catatan` text NOT NULL,
+  `tanggal_update` varchar(40) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `catatan_info_harga`
 --
 
-INSERT INTO `catatan_info_harga` (`catatan`) VALUES
-('1. Harga BELUM termasuk biaya pengiriman (transportasi).\r\n2. Harga sudah termasuk PPN 11% dan upah.\r\n3. Harga dapat berubah sewaktu-waktu sesuai kondisi pasar (antara lain: harga bahan, kurs, dll).\r\n4. Biaya belum termasuk tarif pekerja atau mandah di LUAR SUMATERA UTARA (luar wilayah).\r\n5. Pemesanan selain PTPN IV harus melalui persetujuan BoM (Board of Management).');
+INSERT INTO `catatan_info_harga` (`id`, `catatan`, `tanggal_update`) VALUES
+(1, '1. Harga BELUM termasuk biaya pengiriman (transportasi).\r\n2. Harga sudah termasuk PPN 11% dan upah.\r\n3. Harga dapat berubah sewaktu-waktu sesuai kondisi pasar (antara lain: harga bahan, kurs, dll).\r\n4. Biaya belum termasuk tarif pekerja atau mandah di LUAR SUMATERA UTARA (luar wilayah).\r\n5. Pemesanan selain PTPN IV harus melalui persetujuan BoM (Board of Management).', 'Jumat, 30 Desember 2022 pukul 05.48 WIB');
 
 -- --------------------------------------------------------
 
@@ -87,23 +89,22 @@ INSERT INTO `info_harga` (`id`, `banner_harga`) VALUES
 CREATE TABLE `komplain` (
   `id` int(255) NOT NULL,
   `nama` varchar(40) NOT NULL,
-  `email` varchar(40) NOT NULL,
   `tanggal_komplain` varchar(50) NOT NULL,
   `judul_komplain` varchar(60) NOT NULL,
   `isi_komplain` text NOT NULL,
   `gambar` varchar(100) DEFAULT NULL,
   `id_balasan` int(255) DEFAULT NULL,
-  `is_hidden` tinyint(1) NOT NULL DEFAULT 0
+  `is_hidden` tinyint(1) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `komplain`
 --
 
-INSERT INTO `komplain` (`id`, `nama`, `email`, `tanggal_komplain`, `judul_komplain`, `isi_komplain`, `gambar`, `id_balasan`, `is_hidden`) VALUES
-(54, 'Prayogi Wiranata', 'prayogi@pmt.id', 'Selasa, 27 Desember 2022 pukul 05.15 WIB', 'Barang Tidak Sesuai', 'Barang yang saya terima tidak sesuai dengan yang saya pesan. Nomor pesanan saya adalah PSP-3221/INV0062381/PMT.\r\nMohon ditindaklanjuti. Terima kasih.', 'Prayo_1672114522-1737', 54, 0),
-(55, 'Agus Wijaya', 'aguswijaya@gmail.com', 'Selasa, 27 Desember 2022 pukul 05.18 WIB', 'Barang Belum Sampai', 'Barang yang saya pesan belum sampai. Sudah 1 minggu menunggu.', 'AgusW_1672114692-1128', 55, 1),
-(56, 'William Stallings', 'william.stalling@me.id', 'Selasa, 27 Desember 2022 pukul 05.46 WIB', 'Barang Kurang ', 'Barang yang saya terima jumlahnya berbeda dengan yang saya pesan.', NULL, 56, 0);
+INSERT INTO `komplain` (`id`, `nama`, `tanggal_komplain`, `judul_komplain`, `isi_komplain`, `gambar`, `id_balasan`, `is_hidden`) VALUES
+(54, 'Prayogi Wiranata', 'Selasa, 27 Desember 2022 pukul 05.15 WIB', 'Barang Tidak Sesuai', 'Barang yang saya terima tidak sesuai dengan yang saya pesan. Nomor pesanan saya adalah PSP-3221/INV0062381/PMT.\r\nMohon ditindaklanjuti. Terima kasih.', 'Prayo_1672114522-1737', 54, 0),
+(55, 'Agus Wijaya', 'Selasa, 27 Desember 2022 pukul 05.18 WIB', 'Barang Belum Sampai', 'Barang yang saya pesan belum sampai. Sudah 1 minggu menunggu.', 'AgusW_1672114692-1128', 55, 1),
+(56, 'William Stallings', 'Selasa, 27 Desember 2022 pukul 05.46 WIB', 'Barang Kurang ', 'Barang yang saya terima jumlahnya berbeda dengan yang saya pesan.', NULL, 56, 0);
 
 -- --------------------------------------------------------
 
@@ -142,7 +143,7 @@ CREATE TABLE `stok_sparepart_pmt` (
 --
 
 INSERT INTO `stok_sparepart_pmt` (`id`, `tanggal_update`, `lori_rebusan`, `fruit_elevator`, `as_thresher`, `tromol_thresher`, `body_cbc`, `gantungan_cbc`, `pedal_cbc`, `body_polishdrum`, `roll_body_polishdrum`, `roll_bawah_polishdrum`, `gear_polishdrum`, `dewatering_drum`, `bottom_cone_inti`, `bottom_cone_cangkang`, `vortex_finder`, `feed_housing`, `body_cyclone`, `separating_cyclone`, `box_control`, `roda_lori`, `bushing_lori`) VALUES
-(1, 'Rabu, 28 Desember 2022 pukul 15.26 WIB', 50, 1, 2, 1, 0, 20, 0, 1, 0, 0, 2, 2, 0, 1, 2, 0, 0, 0, 0, 20, 0),
+(1, 'Jumat, 30 Desember 2022 pukul 06.04 WIB', 50, 1, 2, 1, 0, 20, 0, 1, 0, 0, 2, 2, 0, 1, 2, 0, 0, 0, 0, 20, 0),
 (2, 'Jumat, 28 Oktober 2022 pukul 11.35 WIB', 92, 21, 21, 2215, 2123, 213, 312, 21, 31, 21, 31, 21, 31, 22, 32, 21, 22, 21, 22, 0, 0);
 
 -- --------------------------------------------------------
@@ -155,7 +156,7 @@ CREATE TABLE `user` (
   `id` int(8) NOT NULL,
   `nama` varchar(45) NOT NULL,
   `email` varchar(45) NOT NULL,
-  `password` varchar(160) NOT NULL,
+  `password` varchar(120) NOT NULL,
   `profile_pic` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -169,6 +170,12 @@ INSERT INTO `user` (`id`, `nama`, `email`, `password`, `profile_pic`) VALUES
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `catatan_info_harga`
+--
+ALTER TABLE `catatan_info_harga`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `info_harga`
@@ -199,10 +206,16 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT for table `catatan_info_harga`
+--
+ALTER TABLE `catatan_info_harga`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `komplain`
 --
 ALTER TABLE `komplain`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=61;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=68;
 
 --
 -- AUTO_INCREMENT for table `stok_sparepart_pmt`
