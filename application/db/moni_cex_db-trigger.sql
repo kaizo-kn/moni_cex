c7b20a9259909e71c3d95df767228bd19ec0c411
asus <asusduo@gmail.com>
2023-02-22 15:55:45 +0700

    jhj

---
+++ application/db/moni_cex_db-trigger.sql
@@ -0,0 +1,70 @@
+
+/*insert*/
+DELIMITER $$
+CREATE TRIGGER insert_up_dokumen
+   AFTER INSERT
+    ON uraian_pekerjaan
+    FOR EACH ROW 
+BEGIN
+    INSERT INTO `dokumen`(`id_pekerjaan`) VALUES (NEW.id_pekerjaan);
+END$$
+DELIMITER ;
+
+
+
+DELIMITER $$
+CREATE TRIGGER insert_up_persentase
+   AFTER INSERT
+    ON uraian_pekerjaan
+    FOR EACH ROW 
+BEGIN
+   INSERT INTO `persentase_progress`(`id_pekerjaan`,`tanggal`, `persentase`) VALUES (NEW.id_pekerjaan,NEW.tanggal,0);
+END$$
+DELIMITER ;
+
+
+DELIMITER $$
+CREATE TRIGGER insert_up_dokumentasi
+   AFTER INSERT
+    ON uraian_pekerjaan
+    FOR EACH ROW 
+BEGIN
+   INSERT INTO `dokumentasi`(`id_pekerjaan`) VALUES (NEW.id_pekerjaan);
+END$$
+DELIMITER ;
+
+
+
+/*delete*/
+DELIMITER $$
+CREATE TRIGGER delete_up_dokumen
+   AFTER DELETE
+    ON uraian_pekerjaan
+    FOR EACH ROW 
+BEGIN
+DELETE FROM  `dokumen` WHERE `id_pekerjaan` = OLD.id_pekerjaan;
+END$$
+DELIMITER ;
+
+
+
+DELIMITER $$
+CREATE TRIGGER delete_up_persentase
+   AFTER DELETE
+    ON uraian_pekerjaan
+    FOR EACH ROW 
+BEGIN
+   DELETE FROM `persentase_progress` WHERE `id_pekerjaan` = OLD.id_pekerjaan;
+END$$
+DELIMITER ;
+
+
+DELIMITER $$
+CREATE TRIGGER delete_up_dokumentasi
+   AFTER DELETE
+    ON uraian_pekerjaan
+    FOR EACH ROW 
+BEGIN
+   DELETE FROM `dokumentasi` WHERE `id_pekerjaan` = OLD.id_pekerjaan;
+END$$
+DELIMITER ;
