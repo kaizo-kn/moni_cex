a462103fe0ee91af23279e7a859f597fa71e8345
kaizo kn <kaizo_kn@gmail.com>
2023-03-06 10:05:19 +0700

    Changes to be committed: 	modified:   application/views/__partials/menu.php

--- application/views/__partials/menu.php
+++ application/views/__partials/menu.php
@@ -46,60 +46,89 @@
     }
     ?>
     <!-- Offcanvas -->
-    <div class="offcanvas offcanvas-start rounded offcanvas-bg" tabindex="-1" id="offcanvasWithBackdrop"
+    <div class="offcanvas offcanvas-start rounded" tabindex="-1" id="offcanvasWithBackdrop"
         aria-labelledby="offcanvasWithBackdropLabel">
         <div class="offcanvas-header">
-            <h1 class="logo me-auto"><a class="text-main" href="<?= base_url('index.php/'); ?>">Menu: </a></h1>
+            <h1 class="logo me-auto">
+                <p class="text-main">Menu: </p>
+            </h1>
             <button style="border: solid 1px white;border-radius: 5px;" type="button" class=" btn text-dark"
                 data-bs-dismiss="offcanvas" aria-label="Close"><i class="bi bi-chevron-left text-dark"></i></button>
         </div>
 
         <div class="offcanvas-body text-dark">
-            <a href="<?= base_url("index.php/{$role}/"); ?>" class="<?php if (isset($m1)) {
-                                                                        echo $m1;
-                                                                    }  ?>">
-                <div id="menu-home" class="d-flex align-items-center p-1">
-                    <div style="vertical-align: middle;" class="d-table-cell">
-                        <span class="bi bi-speedometer fw-bold fs-2"></span><span
-                            class="ps-3 fw-bold mb-3">Dashboard</span>
+            <div class="d-none d-sm-block d-md-block d-lg-none d-xl-none">
+                <a href="<?= base_url("index.php/{$role}/"); ?>" class="<?php if (isset($m1)) {
+                                                                            echo $m1;
+                                                                        }  ?>">
+                    <div id="menu-home" class="d-flex align-items-center p-1">
+                        <div style="vertical-align: middle;" class="d-table-cell">
+                            <span class="bi bi-speedometer fw-bold fs-2"></span><span
+                                class="ps-3 fw-bold mb-3">Dashboard</span>
+                        </div>
                     </div>
-                </div>
-            </a>
-            <a href="<?= base_url("index.php/$role/lap_invest") ?>" class="<?php if (isset($m2)) {
-                                                                                echo $m2;
-                                                                            }  ?>">
-                <div id="menu-info-produk" class="d-flex align-items-center p-1">
-                    <div style="vertical-align: middle;" class="d-table-cell">
-                        <span class="bi bi-cone-striped fw-bold fs-2"></span><span class="ps-3 fw-bold mb-3">Progress
-                            Lap. Investasi</span>
+                </a>
+                <a href="<?= base_url("index.php/$role/lap_invest") ?>" class="<?php if (isset($m2)) {
+                                                                                    echo $m2;
+                                                                                }  ?>">
+                    <div id="menu-info-produk" class="d-flex align-items-center p-1">
+                        <div style="vertical-align: middle;" class="d-table-cell">
+                            <span class="bi bi-cone-striped fw-bold fs-2"></span><span
+                                class="ps-3 fw-bold mb-3">Progress
+                                Lap. Investasi</span>
+                        </div>
                     </div>
-                </div>
-            </a>
-            <a href="<?= base_url("index.php/$role/pengawasan_pekerjaan_lap") ?>" class="<?php if (isset($m3)) {
-                                                                                                echo $m3;
-                                                                                            }  ?>">
-                <div id="menu-info-harga" class="d-flex align-items-center p-1">
-                    <div style="vertical-align: middle;" class="d-table-cell">
-                        <span class="bi bi-binoculars-fill fw-bold fs-2"></span><span
-                            class="ps-3 fw-bold mb-3">Pengawasan Pekerjaan Lap.</span>
+                </a>
+                <a href="<?= base_url("index.php/$role/pengawasan_pekerjaan_lap") ?>" class="<?php if (isset($m3)) {
+                                                                                                    echo $m3;
+                                                                                                }  ?>">
+                    <div id="menu-info-harga" class="d-flex align-items-center p-1">
+                        <div style="vertical-align: middle;" class="d-table-cell">
+                            <span class="bi bi-binoculars-fill fw-bold fs-2"></span><span
+                                class="ps-3 fw-bold mb-3">Pengawasan Pekerjaan Lap.</span>
+                        </div>
                     </div>
-                </div>
-            </a>
+                </a>
+            </div>
+            <div class="mt-2">
+                <?php
+                if ($role == 'admin') {
+                    echo '
+            <a class="mb-3 main-text-color" href="' . site_url("admin/input_pekerjaan") . '">Input Uraian Pekerjaan</a><br>
+            <a class="mb-3 main-text-color" href="' . site_url("admin/update_progress") . '">Update Progress</a><br>
+            <a class="mb-3 main-text-color" href="' . site_url("admin/upload_dokumen") . '">Upload Dokumen</a><br>
+            <a class="mb-3 main-text-color" href="' . site_url("admin/hapus_pekerjaan") . '">Hapus Uraian Pekerjaan</a><br>
+            <a class="mb-3 main-text-color" href="' . site_url("admin/reset_user") . '">Reset Password User</a><br>';
+                } else {
+                    echo '<a class="mb-3 main-text-color" href="' . site_url("user/input_progress_lap") . '">Input Progress Lap. Invest.</a><br>
+            <a class="mb-3 main-text-color" href="' . site_url("user/input_pengawasan_lap") . '"><small>Upload Dokumen Pengawasan Pekerjaan Lap.</small></a><br>
+            ';
+                } ?>
+                <a class="mb-3 main-text-color" href="<?php echo site_url("$role/profile") ?>">Ubah Profil</a><br>
+                <a class="mb-3 main-text-color" href="<?php echo site_url('login/logout') ?>">Keluar</a><br>
+            </div>
+
         </div>
     </div>
     <!-- End Offcanvas -->
 
     <!-- ======= Header ======= -->
     <header style="position: sticky;" id="header" class="header-scrolled ">
-        <div style="min-height:3rem;" class="container-fluid pe-2 d-inline-flex justify-content-between">
-            <h1 class="logo d-none d-lg-block d-xl-block d-md-none align-items-center "><img
-                    src="<?= base_url() ?>assets/img/icons/Logo_PTPN4.png" alt="" sizes="160x160" srcset=""
-                    class="me-2"><a class="text-center mt-2 logo-title align-items-center me-5 btm-border fromCenter"
-                    href="<?= base_url('index.php/') ?>">MONI-CEX </a></h1>
 
-            <span data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBackdrop"
-                aria-controls="offcanvasWithBackdrop" class="fs-3 text-dark"><i style="font-weight: bold;"
-                    class="bi bi-list text-light pt-5 curpo pb-5"></i></span>
+        <div style="min-height:3rem;" class="container-fluid pe-2 d-inline-flex justify-content-between">
+            <div class="d-inline">
+                <span class="logo d-none d-lg-block d-xl-block d-md-none align-items-center mt-2"><img
+                        src="<?= base_url() ?>assets/img/icons/Logo_PTPN4.png" alt="" sizes="160x160" srcset=""
+                        class="me-2"><strong class="text-light fs-5 mt-5 pt-2 me-3">MONI-CEX</strong><span
+                        data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBackdrop"
+                        aria-controls="offcanvasWithBackdrop" class="fs-3 mt-3"><i style="font-weight: bold;"
+                            class="bi bi-list text-light mt-5 pt-5 curpo pb-5"></i></span>
+                </span>
+
+            </div>
+
+
+
             <nav id="navbar" class="navbar">
                 <?php
                 $type = "";
