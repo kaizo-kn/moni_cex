7a143c4ec4ebd213e91ee6bbae402523536de20b
kaizo kn <kaizo_kn@gmail.com>
2023-03-08 12:15:20 +0700

    fix unresponsive table

--- application/views/user/pengawasan_pekerjaan_lap.php
+++ application/views/user/pengawasan_pekerjaan_lap.php
@@ -3,9 +3,8 @@
         <div class="section-title mt-5 ">
             <h2>pengawasan pekerjaan lapangan</h2>
         </div>
-        <div style="width: 100%;">
-            <table style="table-layout: fixed;width:100%" id="tabel_lap_invest"
-                class="table table-hover table-bordered w-100">
+        <div class="table-responsive-md table-responsive-sm table-responsive-lg" style="width: 100%;">
+            <table style="table-layout: fixed;width:100%" id="tabel_lap_invest" class="table table-hover table-bordered w-100">
                 <div class="row justify-content-end">
 
 
@@ -83,18 +82,18 @@
 </section>
 
 <script>
-let tabel_lap_invest = $('#tabel_lap_invest').DataTable({
-    "language": {
-        "url": "<?= base_url() ?>assets/vendor/datatables/js/indonesian.json"
-    },
-    columnDefs: [{
-        targets: [3, 4, 5, 6, 7],
-        orderable: false
-    }],
-    pageLength: 25
-});
+    let tabel_lap_invest = $('#tabel_lap_invest').DataTable({
+        "language": {
+            "url": "<?= base_url() ?>assets/vendor/datatables/js/indonesian.json"
+        },
+        columnDefs: [{
+            targets: [3, 4, 5, 6, 7],
+            orderable: false
+        }],
+        pageLength: 25
+    });
 
-let filtertype = `<div id="filter_type" class="form-inline float-start me-4 pe-3 d-none d-lg-block d-xl-block">
+    let filtertype = `<div id="filter_type" class="form-inline float-start me-4 pe-3 d-none d-lg-block d-xl-block">
             <span class="ms-2 text-dark"><label for="pks" ><input onchange="filterType()" style="transform:translateY(25%);" type="checkbox" checked name="" value="pks" id="pks" class="form-check curpo d-inline"><small class="me-1 curpo ms-1 fw-bolder">PKS</small></label></span>
             <span class="ms-2 text-danger"><label for="tekpol" ><input onchange="filterType()" style="transform:translateY(25%);" type="checkbox" checked name="" value="tekpol" id="tekpol" class="form-check curpo d-inline"><small class="me-1 curpo ms-1 fw-bolder">TEKPOL</small></label></span>
             <span class="ms-2 text-orange"><label for="hps" ><input onchange="filterType()" style="transform:translateY(25%);" type="checkbox" checked name="" value="hps" id="hps" class="form-check curpo d-inline"><small class="me-1 curpo ms-1 fw-bolder">HPS</small></label></span>
@@ -105,37 +104,37 @@ let filtertype = `<div id="filter_type" class="form-inline float-start me-4 pe-3
 
 
 
-$(document).ready(function() {
+    $(document).ready(function() {
 
-    setTimeout(() => {
-        $('#tabel_lap_invest_filter').append(filtertype);
-        $('#tabel_lap_invest_filter').addClass("mb-3 me-3");
-    }, 100);
-});
+        setTimeout(() => {
+            $('#tabel_lap_invest_filter').append(filtertype);
+            $('#tabel_lap_invest_filter').addClass("mb-3 me-3");
+        }, 100);
+    });
 
-function setDownloadImage(url) {
-    let elem = `<a href="${url}" class="bi bi-download fw-bold ms-3" download></a>`
-    setTimeout(() => {
-        $('.gslide-title').append(elem)
-    }, 100);
-}
-
-function filterType() {
-    let res = ""
-    let filter_value = $('#filter_type input:checked').map(function() {
-        return $(this).val();
-    }).get()
-    if (filter_value.length < 1) {
-        res = 'x'
+    function setDownloadImage(url) {
+        let elem = `<a href="${url}" class="bi bi-download fw-bold ms-3" download></a>`
+        setTimeout(() => {
+            $('.gslide-title').append(elem)
+        }, 100);
     }
 
-    for (let index = 0; index < filter_value.length; index++) {
-        if (filter_value.length == (index + 1)) {
-            res += filter_value[index];
-        } else {
-            res += filter_value[index] + "|";
+    function filterType() {
+        let res = ""
+        let filter_value = $('#filter_type input:checked').map(function() {
+            return $(this).val();
+        }).get()
+        if (filter_value.length < 1) {
+            res = 'x'
         }
+
+        for (let index = 0; index < filter_value.length; index++) {
+            if (filter_value.length == (index + 1)) {
+                res += filter_value[index];
+            } else {
+                res += filter_value[index] + "|";
+            }
+        }
+        tabel_lap_invest.columns(3).search(`${res}`, true, false).draw();
     }
-    tabel_lap_invest.columns(3).search(`${res}`, true, false).draw();
-}
 </script>
